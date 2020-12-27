0832aab8cd51f0f7deb59e0b18570f4ff1056541
ilayofir <ilayofir@gmail.com>
2020-12-28 00:18:32 +0200

    demo

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,10 +1,8 @@
 import React, { useState, useEffect, useRef } from "react";
-import { withKnobs, boolean, number, array } from '@storybook/addon-knobs';
-import AbortController from "abort-controller"
-import GridTable from '../src';
+import ReactDOM from "react-dom";
+import GridTable from '../../src';
 import MOCK_DATA from "./MOCK_DATA.json";
-import Username from "./components/Username";
-import './index.css';
+// import './index.css';
 
 const EDIT_SVG = <svg height="16" viewBox="0 0 20 20" width="16" xmlns="http://www.w3.org/2000/svg"><g fill="#fff" stroke="#1856bf" transform="translate(2 2)"><path d="m8.24920737-.79402796c1.17157287 0 2.12132033.94974747 2.12132033 2.12132034v13.43502882l-2.12132033 3.5355339-2.08147546-3.495689-.03442539-13.47488064c-.00298547-1.16857977.94191541-2.11832105 2.11049518-2.12130651.00180188-.00000461.00360378-.00000691.00540567-.00000691z" transform="matrix(.70710678 .70710678 -.70710678 .70710678 8.605553 -3.271644)"/><path d="m13.5 4.5 1 1"/></g></svg>;
 const CANCEL_SVG = <svg height="20" viewBox="0 0 20 20" width="20" xmlns="http://www.w3.org/2000/svg"><g fill="none" stroke="#dc1e1e" transform="translate(5 5)"><path d="m.5 10.5 10-10"/><path d="m10.5 10.5-10-10z"/></g></svg>;
@@ -18,111 +16,6 @@ const styles = {
 	cancelButton: {background: '#f3f3f3', outline: 'none', cursor: 'pointer', marginRight: 10, padding: 2, display: 'inline-flex', border: 'none', borderRadius: '50%', boxShadow: '1px 1px 2px 0px rgb(0 0 0 / .3)'},
 	saveButton: {background: '#f3f3f3', outline: 'none', cursor: 'pointer', padding: 2, display: 'inline-flex', border: 'none', borderRadius: '50%', boxShadow: '1px 1px 2px 0px rgb(0 0 0 / .3)'}
 }
-
-const baseColumns = [
-    {
-        id: 'checkbox',
-        pinned: true,
-        width: '54px',
-        label: 'Select'
-    },
-    {
-        id: 'id',
-        field: 'id',
-        label: 'id',
-    },
-    {
-        id: 2,
-        field: 'username',
-        label: 'Username',
-        cellRenderer: Username,
-        editorCellRenderer: props => <Username {...props} isEdit />
-    },
-    {
-        id: 3,
-        field: 'first_name',
-        label: 'First Name'
-    },
-    {
-        id: 4,
-        field: 'last_name',
-        label: 'Last Name'
-    },
-    {
-        id: 5,
-        field: 'email',
-        label: 'Email'
-    },
-    {
-        id: 6,
-        field: 'gender',
-        label: 'Gender',
-        editorCellRenderer: ({ tableManager, value, onChange, data, column, rowIndex }) => (
-            <select
-                style={styles.select}
-                value={value}
-                onChange={e => onChange({ ...data, [column.field]: e.target.value })}
-            >
-                <option>Male</option>
-                <option>Female</option>
-            </select>
-        )
-    },
-    {
-        id: 7,
-        field: 'ip_address',
-        label: 'IP Address'
-    },
-    {
-        id: 8,
-        field: 'last_visited',
-        label: 'Last Visited',
-        sort: ({ a, b, isAscending }) => {
-            let aa = a.split('/').reverse().join(),
-                bb = b.split('/').reverse().join();
-            return aa < bb ? isAscending ? -1 : 1 : (aa > bb ? isAscending ? 1 : -1 : 0);
-        }
-    },
-    {
-        id: 9,
-        width: 'max-content',
-        pinned: true,
-        sortable: false,
-        resizable: true,
-        cellRenderer: ({ tableManager, value, data, column, rowIndex, searchText }) => (
-            <div style={styles.buttonsCellContainer}>
-                <button
-                    title="Edit"
-                    style={styles.editButton}
-                    onClick={e => tableManager.rowEditApi.setEditRowId(data.id)}
-                >
-                    {EDIT_SVG}
-                </button>
-            </div>
-        ),
-        editorCellRenderer: ({ onRowEditSave, tableManager, value, field, onChange, data, column, rowIndex }) => (
-            <div style={styles.buttonsCellEditorContainer}>
-                <button
-                    title="Cancel"
-                    style={styles.cancelButton}
-                    onClick={e => tableManager.rowEditApi.setEditRowId(null)}
-                >
-                    {CANCEL_SVG}
-                </button>
-                <button
-                    title="Save"
-                    style={styles.saveButton}
-                    onClick={e => {
-                        onRowEditSave(data);
-                        tableManager.rowEditApi.setEditRowId(null);
-                    }}
-                >
-                    {SAVE_SVG}
-                </button>
-            </div>
-        )
-    }
-]
 
 const MyAwesomeTable = () => {
     const [editRowId, setEditRowId] = useState(null);
@@ -145,13 +38,13 @@ const MyAwesomeTable = () => {
             field: 'id',
             label: 'id',
         },
-        {
-            id: 2,
-            field: 'username',
-            label: 'Username',
-            cellRenderer: Username,
-            editorCellRenderer: props => <Username {...props} isEdit />
-        },
+        // {
+        //     id: 2,
+        //     field: 'username',
+        //     label: 'Username',
+        //     cellRenderer: Username,
+        //     editorCellRenderer: props => <Username {...props} isEdit />
+        // },
         {
             id: 3,
             field: 'first_name',
@@ -274,3 +167,5 @@ const MyAwesomeTable = () => {
 }
 
 export default MyAwesomeTable;
+
+ReactDOM.render(<MyAwesomeTable />, document.getElementById('root'));
