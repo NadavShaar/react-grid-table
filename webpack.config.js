dc7288c16af829610e01779449b1489c26447942
Jae <irrisjay@gmail.com>
2021-03-11 13:10:28 +0800

    Directly includes core-js in @babel/preset-env as per https://babeljs.io/docs/en/babel-preset-env#usebuiltins
    Unbreaks the component for React setups without global core-js

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                        presets: [['@babel/preset-env', {"useBuiltIns": "usage", "corejs": 3}], '@babel/preset-react'],
                         plugins: ['@babel/plugin-proposal-object-rest-spread']
                     }
                 },
